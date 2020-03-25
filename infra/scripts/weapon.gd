eaf7f353d76d94af6de84ce4e3b3c6b791251550
James Desjardins <jamesdesjardins@protonmail.com>
2020-03-25 19:57:39 -0400

    Weapon connects the item scenes hitbox

--- infra/scripts/weapon.gd
+++ infra/scripts/weapon.gd
@@ -32,6 +32,8 @@ var trigger: Area2D
 var hitbox_ignored_node: Node
 
 func _ready():
+	# read json file at json_path
+	# parse into dictionary
 	var file = File.new()
 	assert(file.file_exists(json_path))
 	file.open(json_path, 1)
@@ -40,17 +42,21 @@ func _ready():
 	
 	var parsed_dict = parse_result.result
 	
+	# instance the scene located at item_scene
 	assert(parsed_dict.has("item_scene"))
 	var item_scene = load(parsed_dict.item_scene)
 	var instanced_item = item_scene.instance()
 	add_child(instanced_item)
 	
+	# get the sprite from the item scene
 	assert(instanced_item.has_node("Sprite"))
 	sprite = instanced_item.get_node("Sprite")
 	
+	# get the trigger from the item scene
 	assert(instanced_item.has_node("Trigger"))
 	trigger = instanced_item.get_node("Trigger")
 	
+	# connect the item trigger to be able to be picked up
 	var err = trigger.connect("body_entered", self, "trigger_entered")
 	if err != OK:
 		print("Error initializing Weapon class")
@@ -59,17 +65,39 @@ func _ready():
 	if err != OK:
 		print("Error initializing Weapon class")
 	
+	# connect the items hitbox to a deal damage function
+	# right now assume the root of the item is an Area2D
+	assert(instanced_item is Area2D)
+	hitbox = instanced_item
+	err = hitbox.connect("body_entered", self, "hit_body")
+	err = hitbox.connect("area_entered", self, "hit_area")
+	
+	# parse the attack definitions (animations and durations)
 	assert(parsed_dict.has("attacks"))
 	var attacks = parsed_dict.attacks
 	for key in parsed_dict.attacks.keys():
 		var aa = AttackAnimation.new(attacks[key])
 		attack_animations[key] = aa
+
+func hit_body(_body):
+	print("Should hit_body even be called???")
+
+func hit_area(area):
+	var hitbox_bit = 1
+	var hurtbox_bit = 2
 	
+	if area == hitbox_ignored_node:
+		return
 	
+	if area.get_collision_layer_bit(hurtbox_bit):
+		print("Dealing damage to ", area.get_path())
+	elif area.get_collision_layer_bit(hitbox_bit):
+		print("Clashing with other hitbox ", area.get_path())
+
 # TODO: set_ignore(entity.hurtbox)
 func held_by(entity: Node):
+	# set the trigger to ignore the entity
 	set_ignore(entity)
-	print(entity.hurtbox.get_path())
 	hitbox_ignored_node = entity.hurtbox
 	trigger.get_node("CollisionShape2D").disabled = true
 
