33793318e55bb488f7106973f8c1dbed92913cea
James Desjardins <jamesdesjardins@protonmail.com>
2020-04-15 13:00:46 -0400

    resize viewport based on minimum size

--- infra/main.gd
+++ infra/main.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
 onready var root = get_tree().get_root()
-export(Vector2) var max_size
-onready var current_scale = 1.0
+export(Vector2) var min_size
+var viewport_transform: Transform2D
+
+var size_cap: float
 
 func _ready():
+	assert($"Viewport".size.x == $"Viewport".size.y)
+	assert(min_size.x == min_size.y)
+
+	size_cap = $"Viewport".size.x
+
 	root.connect("size_changed", self, "window_resize")
 	window_resize()
 
@@ -12,26 +19,38 @@ func window_resize():
 	var window_size = OS.get_window_size()
 	root.set_size_override(true, window_size)
 
-	var float_scale = window_size / max_size
+	var fscale = window_size / min_size
+	var current_scale = min(floor(fscale.x), floor(fscale.y))
 
-	current_scale = max(ceil(float_scale.x), ceil(float_scale.y))
+	var viewport_size = window_size / current_scale
+	viewport_size.x = ceil(viewport_size.x)
+	viewport_size.y = ceil(viewport_size.y)
 
-	var rounded_scale_vec = Vector2(current_scale, current_scale)
+	var current_position = Vector2(0, 0)
 
-	var sub_viewport_size = window_size / rounded_scale_vec
+	if viewport_size.x > size_cap:
+		current_position.x = floor((viewport_size.x - size_cap) * current_scale / 2)
+		viewport_size.x = size_cap
 
-	$"Viewport".set_size_override(true, sub_viewport_size)
-	$"ViewportSprite".scale = rounded_scale_vec
+	if viewport_size.y > size_cap:
+		current_position.y = floor((viewport_size.y - size_cap) * current_scale / 2)
+		viewport_size.y = size_cap
+
+	$"Viewport".set_size_override(true, viewport_size)
+
+	viewport_transform = Transform2D.IDENTITY.scaled(Vector2(current_scale, current_scale)).translated(current_position)
+	$"ViewportSprite".transform = viewport_transform
 
 func _input(event):
 	# TODO: events related to screen space must be translated to the sub viewport
 
 	if event is InputEventMouse:
-		event.global_position /= current_scale
-		event.position /= current_scale
+		var affine_inverse = viewport_transform.affine_inverse()
+		event.global_position = affine_inverse.xform(event.global_position)
+		event.position = affine_inverse.xform(event.position)
 
-	if event is InputEventMouseMotion:
-		event.relative /= current_scale
-		event.speed /= current_scale
+		if event is InputEventMouseMotion:
+			event.relative = affine_inverse.xform(event.relative)
+			event.speed = affine_inverse.xform(event.speed)
 
 	$"Viewport".input(event)
