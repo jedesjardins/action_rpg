5a0c02784b8d8712c2a256772858ff4c58e8d53a
James Desjardins <jamesdesjardins@protonmail.com>
2020-04-16 15:40:32 -0400

    Add the title card

--- inst/scenes/main_menu_scene/main_menu.gd
+++ inst/scenes/main_menu_scene/main_menu.gd
@@ -3,8 +3,8 @@ extends Node2D
 onready var focused = false
 
 func _ready():
-	var _err = $"CanvasLayer/CenterContainer/VBoxContainer/NewGameButton".connect("button_down", self, "new_game")
-	_err = $"CanvasLayer/CenterContainer/VBoxContainer/QuitGameButton".connect("button_down", self, "quit_game")
+	var _err = $"CanvasLayer/CenterContainer/VBoxContainer/VBoxContainer/NewGameButton".connect("button_down", self, "new_game")
+	_err = $"CanvasLayer/CenterContainer/VBoxContainer/VBoxContainer/QuitGameButton".connect("button_down", self, "quit_game")
 
 func new_game():
 	var game_scene = load("res://inst/scenes/basic_scene/basic_scene.tscn").instance()
@@ -15,5 +15,5 @@ func quit_game():
 
 func _unhandled_input(event):
 	if (event is InputEventKey or event is InputEventAction) and not focused:
-		$"CanvasLayer/CenterContainer/VBoxContainer/NewGameButton".grab_focus()
+		$"CanvasLayer/CenterContainer/VBoxContainer/VBoxContainer/NewGameButton".grab_focus()
 		focused = true
